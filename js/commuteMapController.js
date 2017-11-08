faf577391acff25d322d52fb0dd0c800bd9e6818
trishala1234 <tktrishala6@gmail.com>
2017-11-08 16:16:03 +0530

    updated_8/11/2017

--- js/commuteMapController.js
+++ js/commuteMapController.js
@@ -1,9 +1,84 @@
-myApp.controller("commuteMapController", ['$scope', '$state', 'uiGmapGoogleMapApi', commuteMapController]);
+myApp.controller("commuteMapController", ['$scope', '$state', 'uiGmapIsReady', commuteMapController]);
 
-function commuteMapController($scope, $state, uiGmapGoogleMapApi){
-	  $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4, bounds: {}};
+function commuteMapController($scope, $state, uiGmapIsReady){
+	  $scope.map = {control : {}, center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4, bounds: {}};
+	  
         $scope.polylines = [];
-        uiGmapGoogleMapApi.then(function(){
+       uiGmapIsReady.promise().then(function(map_instances){
+       		debugger;
+        	var map = $scope.map.control.getGMap();
+        	var map2 = map_instances[0].map;
+
+        	function AutocompleteDirectionsHandler(map) {
+		        this.map = map;
+		        this.originPlaceId = null;
+		        this.destinationPlaceId = null;
+		        this.travelMode = 'WALKING';
+		        var originInput = document.getElementById('origin-input');
+		        var destinationInput = document.getElementById('destination-input');
+		        //var modeSelector = document.getElementById('mode-selector');
+		        this.directionsService = new google.maps.DirectionsService;
+		        this.directionsDisplay = new google.maps.DirectionsRenderer;
+		        this.directionsDisplay.setMap(map);
+
+		        var originAutocomplete = new google.maps.places.Autocomplete(
+		            originInput, {placeIdOnly: true});
+		        var destinationAutocomplete = new google.maps.places.Autocomplete(
+		            destinationInput, {placeIdOnly: true});
+
+		        //this.setupClickListener('changemode-walking', 'WALKING');
+		        //this.setupClickListener('changemode-transit', 'TRANSIT');
+		        //this.setupClickListener('changemode-driving', 'DRIVING');
+
+		        this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+		        this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+		        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+		        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+		        //this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+      }
+
+      AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function(autocomplete, mode) {
+        var me = this;
+        autocomplete.bindTo('bounds', this.map);
+        autocomplete.addListener('place_changed', function() {
+          var place = autocomplete.getPlace();
+          if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+          }
+          if (mode === 'ORIG') {
+            me.originPlaceId = place.place_id;
+          } else {
+            me.destinationPlaceId = place.place_id;
+          }
+          me.route();
+        });
+
+      };
+
+      var directionDisplays=[];
+      AutocompleteDirectionsHandler.prototype.route = function() {
+        if (!this.originPlaceId || !this.destinationPlaceId) {
+          return;
+        }
+        var me = this;
+
+        this.directionsService.route({
+          origin: {'placeId': this.originPlaceId},
+          destination: {'placeId': this.destinationPlaceId},
+          travelMode: this.travelMode
+        }, function(response, status) {
+          if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      };
+
+      new AutocompleteDirectionsHandler(map);
+
           $scope.polylines = [
             {
                 id: 1,
